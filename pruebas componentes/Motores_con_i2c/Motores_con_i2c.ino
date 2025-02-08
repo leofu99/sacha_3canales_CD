a759f2ee35363b3f00394d73074cb8ab8c2e040f
leo <leonardo.fuentes@udea.edu.co>
2025-02-08 15:24:54 -0500

    setMotor cycle

--- pruebas componentes/Motores_con_i2c/Motores_con_i2c.ino
+++ pruebas componentes/Motores_con_i2c/Motores_con_i2c.ino
@@ -1,6 +1,9 @@
 #include <Wire.h>   // Incluye librería para interfaz I2C
 #include "PCF8574.h" // Librería para el PCF8574
 
+int motor = 0;  // Variable para el motor
+int direccion = 0;  // Variable para la dirección
+
 enum  MotorState {
   REST,
   DROP,
@@ -30,9 +33,9 @@ Motor motores[] = {
   {true, 5, 6, 6, 0, 0, false, false, CLOSE, 0}
 };
 unsigned long MotorInterval = 2000;
-unsigned long sensorInterval = 100000;
+unsigned long sensorInterval = 50000;
 int waitSteps = 400;
-int dropSteps = 200;
+int dropSteps = 100;
 
 PCF8574 i2cSorter(0x25); // Dirección del PCF8574 de los motores
 PCF8574 i2cHall(0x23); // Dirección del PCF8574 de los sensores Hall
@@ -41,11 +44,12 @@ PCF8574 i2cHall(0x23); // Dirección del PCF8574 de los sensores Hall
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("hola");
   for (int i = 0; i < 6; i++) {
     if (motores[i].isI2c) {  // Si es directo
       i2cSorter.pinMode(motores[i].dir, OUTPUT);
       i2cSorter.pinMode(motores[i].pull, OUTPUT);
-      i2cSorter.digitalWrite(motores[i].dir, LOW);
+      i2cSorter.digitalWrite(motores[i].dir, HIGH);
     } else {
       pinMode(motores[i].dir, OUTPUT);
       pinMode(motores[i].pull, OUTPUT);
@@ -56,6 +60,7 @@ void setup() {
 }
 
 void loop() {
+  readMotorSet();
   unsigned long currentTime = micros();
   for (int i = 0; i < 6; i++) {
     moveMotor(currentTime, i);
@@ -69,10 +74,18 @@ void moveMotor(unsigned long currentTime, int i) {
   if (currentTime - motores[i].previousTime >= MotorInterval) {
 
     motores[i].previousTime = currentTime;
-    if (enableMotor(motores[i])){
-    motores[i].toogle = !(motores[i].toogle);
+    if (enableMotor(motores[i])) {
+      if (motores[i].state == DROP ) {
+        motores[i].count ++;
+      }
+
+      motores[i].toogle = !(motores[i].toogle);
       toogleMotor(i, motores[i].toogle);
-    }
+    } else {
+      if(motores[i].state == WAIT){
+         motores[i].count ++;
+        }
+      }
   }
 
 }
@@ -97,31 +110,42 @@ void readSensors(unsigned long currentTime, int i) {
 }
 
 boolean enableMotor(Motor &motor ) {
-  boolean enable = false;
+  boolean enable;
   switch (motor.state) {
     case REST:
       enable = false;
       break;
     case DROP:
+
       if (motor.count < dropSteps) {
         enable = true;
       }
       else {
+        Serial.println("estado drop -> wait ");
+        Serial.println(motor.state);
+        Serial.println(motor.count);
         motor.state = WAIT;
         enable = false;
       }
       break;
     case WAIT:
+
       if (motor.count < waitSteps) {
         enable = false;
       }
       else {
+        Serial.println("estado wait -> close");
+        Serial.println(motor.state);
+        Serial.println(motor.count);
         motor.state = CLOSE;
         enable = true;
       }
       break;
     case CLOSE:
       if (motor.isAtStartPoint) {
+        Serial.println("estado close -> rest: ");
+        Serial.println(motor.state);
+        Serial.println(motor.count);
         enable = false;
         motor.state = REST;
       }
@@ -130,6 +154,56 @@ boolean enableMotor(Motor &motor ) {
       }
       break;
   }
-  motor.count ++;
+
+  //Serial.println(motor.count);
+  //  Serial.println(motor.state);
+
+
   return enable;
+
+
+}
+
+
+void readMotorSet() {
+  if (Serial.available() > 0) {
+    // Lee la línea de texto
+    String input = Serial.readStringUntil('\n');  // Lee hasta el salto de línea
+
+    // Divide la cadena por el espacio o coma, dependiendo de cómo se envíen los datos
+    int separatorIndex = input.indexOf(',');  // Busca la coma (puedes cambiar el delimitador)
+
+    if (separatorIndex != -1) {
+      // Si encontró la coma, divide la cadena en dos partes
+      motor = input.substring(0, separatorIndex).toInt();
+      direccion = input.substring(separatorIndex + 1).toInt();
+    } else {
+      // Si no encontró coma, se asume que hay un espacio
+      motor = input.substring(0, input.indexOf(' ')).toInt();
+      direccion = input.substring(input.indexOf(' ') + 1).toInt();
+    }
+
+    // Muestra los valores leídos
+    for(int i = 0; i<6; i++){
+      drop(i, direccion);
+    }
+    
+
+    // Aquí puedes realizar alguna acción con los valores de motor y dirección
+  }
+}
+
+void drop(int motor, int dir) {
+  if (dir != 0 && dir != 1) {
+    Serial.println("Error: Dirección inválida");
+    return;  // Sale de la función si la dirección no es 0 o 1
+  }
+  motores[motor].state = DROP;
+  motores[motor].count = 0;
+  if (motores[motor].isI2c) {
+    i2cSorter.digitalWrite(motores[motor].dir, !dir);
+  } else {
+    digitalWrite(motores[motor].dir, dir);
+  }
+
 }
