c32152bdc6b286527bf117906e532bde1f0fac11
leo <leonardo.fuentes@udea.edu.co>
2025-02-19 18:31:53 -0500

    motores i2c+

--- pruebas componentes/6_MOTORES/6_MOTORES.ino
+++ pruebas componentes/6_MOTORES/6_MOTORES.ino
@@ -1,53 +1,74 @@
 #include <Wire.h>   // Incluye librería para interfaz I2C
 #include "PCF8574.h" // Librería para el PCF8574
-
+PCF8574 i2cHall(0x23); // Dirección del PCF8574 de los sensores Hall
 int motorDelay = 1;
-int motores[6][3] = {
-  {0, 4, 5},  // Motor 1: Directo, pines 4 y 5
-  {0, 6, 7},  // Motor 2: Directo, pines 6 y 7
-  {0, 8, 9},  // Motor 3: Directo, pines 8 y 9
-  {1, 0, 1},  // Motor 4: I2C, pines 0 y 1 en PCF8574
-  {1, 2, 4},  // Motor 5: I2C, pines 2 y 4 en PCF8574
-  {1, 5, 6}   // Motor 6: I2C, pines 5 y 6 en PCF8574
+int sensor = 1;
+int motores[6][4] = {
+  {0, 4, 5,0},  // Motor 1: Directo, pines 4 y 5
+  {0, 6, 7,1},  // Motor 2: Directo, pines 6 y 7
+  {0, 8, 9,2},  // Motor 3: Directo, pines 8 y 9
+  {1, 0, 1,4},  // Motor 4: I2C, pines 0 y 1 en PCF8574
+  {1, 2, 4,5},  // Motor 5: I2C, pines 2 y 4 en PCF8574
+  {1, 5, 6,6}   // Motor 6: I2C, pines 5 y 6 en PCF8574
 };
 PCF8574 pcf8574(0x25); // Dirección del PCF8574
 
 
 
 void setup() {
-  for (int i = 0; i < 6; i++) {
+  Serial.begin(9600);
+  
+  for (int i = 0; i < 7; i++) {
+   
     if (motores[i][0] == 0) {  // Si es directo
       pinMode(motores[i][1], OUTPUT);
       pinMode(motores[i][2], OUTPUT);
       digitalWrite(motores[i][1], LOW);
+      digitalWrite(motores[i][2], LOW);
       
     } else {
       pcf8574.pinMode(motores[i][1], OUTPUT);
       pcf8574.pinMode(motores[i][2], OUTPUT);
       pcf8574.digitalWrite(motores[i][1], LOW);
+      pcf8574.digitalWrite(motores[i][2], LOW);
     }
-  }
+     i2cHall.pinMode(motores[i][3], INPUT);
+    
+    }
+
+   
+  
 }
 
 void loop() {
+
+  
   
     for (int i = 0; i < 6; i++) {
     if (motores[i][0] == 0) {  // Si es directo
-      for (int j = 0; j < 200; j++) {
+      /*for (int j = 0; j < 200; j++) {
         digitalWrite(motores[i][2], LOW);
         delay(motorDelay);
         digitalWrite(motores[i][2], HIGH);
         delay(motorDelay);
-        }
+        }*/
     }
     else {
-      for (int j = 0; j < 200; j++) {
+        sensor = i2cHall.digitalRead(motores[i][3]);
+       delay(200);
+       Serial.println(sensor);
+    
+    if(sensor==1){
+      for (int j = 0; j < 2000; j++) {
         pcf8574.digitalWrite(motores[i][2], LOW);
         delay(motorDelay);
         pcf8574.digitalWrite(motores[i][2], HIGH);
         delay(motorDelay);
         }
     }
+    }
   }
+  delay(1000);
+    
 
 }
--- pruebas componentes/Motores_con_i2c/Motores_con_i2c.ino
+++ pruebas componentes/Motores_con_i2c/Motores_con_i2c.ino
@@ -1,6 +1,8 @@
 #include <Wire.h>   // Incluye librería para interfaz I2C
 #include "PCF8574.h" // Librería para el PCF8574
+#include <TimerOne.h> // Librería para temporizadores
 
+volatile bool readSensorsFlag = false; // Bandera para la ISR
 int motor = 0;  // Variable para el motor
 int direccion = 0;  // Variable para la dirección
 
@@ -19,7 +21,7 @@ struct Motor {
   unsigned long previousTime;
   unsigned long previousTimeSensor;
   bool toogle;
-  bool isAtStartPoint;
+  bool finishClose;
   MotorState state;
   int count;
 
@@ -33,19 +35,22 @@ Motor motores[] = {
   {true, 5, 6, 6, 0, 0, false, false, CLOSE, 0}
 };
 unsigned long MotorInterval = 2000;
-unsigned long sensorInterval = 50000;
-int waitSteps = 400;
+unsigned long sensorInterval = 5000;
+int waitSteps = 300;
 int dropSteps = 100;
 
-PCF8574 i2cSorter(0x25); // Dirección del PCF8574 de los motores
-PCF8574 i2cHall(0x23); // Dirección del PCF8574 de los sensores Hall
+PCF8574 i2cSorter(0x20); // Dirección del PCF8574 de los motores
+PCF8574 i2cHall(0x27); // Dirección del PCF8574 de los sensores Hall
 
 
 
 void setup() {
+  //Wire.begin();
+   Wire.setClock(100000); 
   Serial.begin(9600);
-  Serial.println("hola");
+  //Serial.println("hola");
   for (int i = 0; i < 6; i++) {
+     i2cHall.pinMode(motores[i].hall, INPUT);
     if (motores[i].isI2c) {  // Si es directo
       i2cSorter.pinMode(motores[i].dir, OUTPUT);
       i2cSorter.pinMode(motores[i].pull, OUTPUT);
@@ -55,16 +60,28 @@ void setup() {
       pinMode(motores[i].pull, OUTPUT);
       digitalWrite(motores[i].dir, LOW);
     }
-    i2cHall.pinMode(motores[i].hall, INPUT);
+   
   }
+
+  //interrupción
+  Timer1.initialize(40000);  // 50ms
+  Timer1.attachInterrupt(timerISR);
 }
 
 void loop() {
+
+  
+    if (readSensorsFlag) {
+    readSensorsFlag = false; // Reiniciar bandera
+    readAllSensors();
+  }
   readMotorSet();
   unsigned long currentTime = micros();
   for (int i = 0; i < 6; i++) {
     moveMotor(currentTime, i);
-    readSensors(currentTime, i);
+
+
+
   }
 }
 
@@ -72,20 +89,14 @@ void loop() {
 void moveMotor(unsigned long currentTime, int i) {
 
   if (currentTime - motores[i].previousTime >= MotorInterval) {
-
-    motores[i].previousTime = currentTime;
-    if (enableMotor(motores[i])) {
-      if (motores[i].state == DROP ) {
+     if (motores[i].state == WAIT || motores[i].state == DROP) {
         motores[i].count ++;
       }
-
+    motores[i].previousTime = currentTime;
+    if (enableMotor(motores[i])) {
       motores[i].toogle = !(motores[i].toogle);
       toogleMotor(i, motores[i].toogle);
-    } else {
-      if(motores[i].state == WAIT){
-         motores[i].count ++;
-        }
-      }
+    } 
   }
 
 }
@@ -93,24 +104,20 @@ void moveMotor(unsigned long currentTime, int i) {
 void toogleMotor(int i, bool toogle) {
   if (motores[i].isI2c) {
     i2cSorter.digitalWrite(motores[i].pull, toogle ?  HIGH : LOW);
+    //delayMicroseconds(100); // Para evitar saturación del bus
   }
   else {
     digitalWrite(motores[i].pull, toogle ?  HIGH : LOW);
   }
 }
 
-void readSensors(unsigned long currentTime, int i) {
-  if (currentTime - motores[i].previousTimeSensor >= sensorInterval) {
-    motores[i].previousTimeSensor = currentTime;
-    // Leer el sensor Hall desde el PCF8574
-    int sensor = i2cHall.digitalRead(motores[i].hall);
-    // Desactivar el motor si el sensor detecta algo (HIGH)
-    motores[i].isAtStartPoint = (sensor == LOW);
-  }
-}
+
+
+
+
 
 boolean enableMotor(Motor &motor ) {
-  boolean enable;
+  boolean enable = false;
   switch (motor.state) {
     case REST:
       enable = false;
@@ -122,7 +129,7 @@ boolean enableMotor(Motor &motor ) {
       }
       else {
         Serial.println("estado drop -> wait ");
-        Serial.println(motor.state);
+
         Serial.println(motor.count);
         motor.state = WAIT;
         enable = false;
@@ -135,28 +142,29 @@ boolean enableMotor(Motor &motor ) {
       }
       else {
         Serial.println("estado wait -> close");
-        Serial.println(motor.state);
+    
         Serial.println(motor.count);
         motor.state = CLOSE;
+        motor.count = 0; // Reiniciar el contador
         enable = true;
       }
       break;
     case CLOSE:
-      if (motor.isAtStartPoint) {
+      if (motor.finishClose) {
         Serial.println("estado close -> rest: ");
-        Serial.println(motor.state);
+   
         Serial.println(motor.count);
         enable = false;
         motor.state = REST;
+        motor.finishClose = false;
+        
+ 
       }
       else {
         enable = true;
       }
       break;
   }
-
-  //Serial.println(motor.count);
-  //  Serial.println(motor.state);
 
 
   return enable;
@@ -183,22 +191,16 @@ void readMotorSet() {
       direccion = input.substring(input.indexOf(' ') + 1).toInt();
     }
 
-    // Muestra los valores leídos
-    for(int i = 0; i<6; i++){
-      drop(i, direccion);
-    }
-    
-
-    // Aquí puedes realizar alguna acción con los valores de motor y dirección
+      drop(motor, direccion);
   }
 }
 
 void drop(int motor, int dir) {
   if (dir != 0 && dir != 1) {
-    Serial.println("Error: Dirección inválida");
     return;  // Sale de la función si la dirección no es 0 o 1
   }
   motores[motor].state = DROP;
+  
   motores[motor].count = 0;
   if (motores[motor].isI2c) {
     i2cSorter.digitalWrite(motores[motor].dir, !dir);
@@ -206,4 +208,20 @@ void drop(int motor, int dir) {
     digitalWrite(motores[motor].dir, dir);
   }
 
+}
+
+void timerISR() {
+  readSensorsFlag = true; // Activar la bandera
+}
+
+void readAllSensors() {
+  for (int i = 0; i < 6; i++) {
+    if(motores[i].state == CLOSE ){
+    int sensor = i2cHall.digitalRead(motores[i].hall);
+    if (sensor == HIGH || sensor == LOW) {  // Evita valores incorrectos  
+        motores[i].finishClose = (sensor == LOW);
+     
+}
+    }
+  }
 }
